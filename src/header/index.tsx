e9ac0c21ef3c72828206ce4aa8e2832c9815b150
Fernanda Winter <fernanda.winter@hotmail.com>
2022-10-06 17:32:10 -0300

    adding footer, header and hp logo

---
+++ src/header/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../hp-logo.svg';
+import './header.css';
+
+function Header() {
+    return (
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+    );
+  }
+  
+  export default Header;
+  
